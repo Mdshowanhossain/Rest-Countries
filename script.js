ef28d3893d77b3798fffbf69183517a77aa9d454
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-08 09:34:07 +0600

    Rest-Countries

--- script.js
+++ script.js
@@ -4,23 +4,59 @@ fetch('https://restcountries.eu/rest/v2/all')
         displayCountries(countriesData)
     })
 
-
 const displayCountries = (countriesData) => {
     const countriesDiv = document.getElementById("countries");
-    for (let i = 0; i < countriesData.length; i++) {
-        const country = countriesData[i];
+    countriesData.forEach(country => {
         // console.log(country.name)
         const countryDiv = document.createElement("div");
+        countriesDiv.className = "countriesDiv"
+        countryDiv.className = 'countryDiv'
         const countryInfo = `
-        <h2>${country.name}</h2>
-        <p>Capital- ${country.capital}<p>
-        <small>Area- ${country.area}<small>
-        <small>Population- ${country.population}<small>
+        <h2 class="country">${country.name}</h2>
+        <p class="capital">Capital- ${country.capital}<p>
+        <small class="area">Area- ${country.area}<small></br>
+        <small class="population">Population- ${country.population}<small><br/>
+        <button to="showCountry" onclick="displayCountry('${country.name}')" class="details">Details</button>
         `
         countryDiv.innerHTML = countryInfo
         countriesDiv.appendChild(countryDiv)
-    }
+    });
 }
+const displayCountry = name => {
+    // console.log(name)
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(response => response.json())
+        .then(responseData => showCountryDetails((responseData[0])))
+}
+const showCountryDetails = (countriesData) => {
+    // console.log(responseData)
+    const showCountry = document.getElementById("showCountry");
+    const createDiv = document.createElement("div")
+    createDiv.className = "countryDivs"
+    const showInfo = `
+    <h2 class="country">${countriesData.name}</h2>
+    <p class="capital">Capital- ${countriesData.capital}<p>
+    <small class="area">Area- ${countriesData.area}<small></br>
+    <small class="population">Population- ${countriesData.population}<small><br/>
+        <img class="flag" src=${countriesData.flag} alt=""/>
+    `
+    createDiv.innerHTML = showInfo;
+    showCountry.appendChild(createDiv);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,6 +71,7 @@ const displayCountries = (countriesData) => {
 //     .then(countriesData => {
 //         displayCountries(countriesData)
 //     })
+
 
 
 // const displayCountries = (countriesData) => {
@@ -65,3 +102,79 @@ const displayCountries = (countriesData) => {
 
 //     }
 // }
+
+
+// < !--MAP METHOD FOR SHOW DATA-- >
+
+
+// const displayCountries = (countriesData) => {
+//     const countriesDiv = document.getElementById("countries");
+
+//     countriesData.map((country) => {
+//         console.log(country.name)
+//         const countryDiv = document.createElement("div");
+//         countriesDiv.className = "countriesDiv"
+//         countryDiv.className = 'countryDiv'
+//         const countryInfo = `
+//             <h2 class="country">${country.name}</h2>
+//             <p class="capital">Capital- ${country.capital}<p>
+//             <small class="area">Area- ${country.area}<small></br>
+//             <small class="population">Population- ${country.population}<small><br/>
+//             <button class="details">Details</button>
+//             `
+//         countryDiv.innerHTML = countryInfo
+//         countriesDiv.appendChild(countryDiv)
+//     })
+
+
+
+
+        // < !--FOREACH METHOD FOR SHOW DATA-- >
+
+
+    //     countriesData.forEach(country => {
+    //         console.log(country.name)
+    //         const countryDiv = document.createElement("div");
+
+    //         countriesDiv.className = "countriesDiv"
+
+    //         countryDiv.className = 'countryDiv'
+
+    //         const countryInfo = `
+
+    // <h2 class="country">${country.name}</h2>
+    // <p class="capital">Capital- ${country.capital}<p>
+    // <small class="area">Area- ${country.area}<small></br>
+    // <small class="population">Population- ${country.population}<small><br/>
+    // <button class="details">Details</button>
+    // `
+    //         countryDiv.innerHTML = countryInfo
+    //         countriesDiv.appendChild(countryDiv)
+    //     });
+
+
+
+// <!-- FOR LOOP METHOD FOR SHOW DATA -->
+
+
+
+// for (let i = 0; i < countriesData.length; i++) {
+//         const country = countriesData[i];
+//         console.log(country.name)
+//         const countryDiv = document.createElement("div");
+
+//         countriesDiv.className = "countriesDiv"
+
+//         countryDiv.className = 'countryDiv'
+
+//         const countryInfo = `
+
+//         <h2 class="country">${country.name}</h2>
+//         <p class="capital">Capital- ${country.capital}<p>
+//         <small class="area">Area- ${country.area}<small></br>
+//         <small class="population">Population- ${country.population}<small><br/>
+//         <button class="details">Details</button>
+//         `
+//         countryDiv.innerHTML = countryInfo
+//         countriesDiv.appendChild(countryDiv)
+//     } 
